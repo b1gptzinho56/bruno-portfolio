b3e6a547b272363e9c13846656c1bd927c9e03fb
b1gptzinho56 <gptzinho5.6@gmail.com>
2026-09-12 13:59:46 -0300

    Add professional contact email

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // WhatsApp: código do país + DDD + número, somente dígitos.
 // E-mail e GitHub: preencha somente quando desejar exibir os links.
-export const contact = { whatsapp: '', email: '', github: '', instagram: 'https://www.instagram.com/bruno_b1_/' };
+export const contact = { whatsapp: '', email: 'contato.brunoweb@gmail.com', github: '', instagram: 'https://www.instagram.com/bruno_b1_/' };
 export const brand = { name: 'Bruno', role: 'Desenvolvimento web' };
